9a9be24e6cfb91014d53e5cae158228e42eec4af
Sudesha T R <104151479+Sudesha-STR@users.noreply.github.com>
2024-07-23 22:43:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -258,4 +258,4 @@ async def generate_feedback(resume_text: str = Form(...), jd: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.0", port=8000)
